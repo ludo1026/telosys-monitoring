524ed1f5ab06950011207207fd617284765151ac
ludo1026 <ludovic.chaboud@sogeti.com>
2014-07-18 18:57:28 +0200

    circular stack

--- src/main/java/org/telosys/webtools/monitoring/CircularStack.java
+++ src/main/java/org/telosys/webtools/monitoring/CircularStack.java
@@ -1,10 +1,14 @@
 package org.telosys.webtools.monitoring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CircularStack {
 	
 	protected final int size;
 	protected final String[] arrays;
 	protected int nextIndex = 0;
+	protected boolean completed = false;
 	
 	public CircularStack(int size) {
 		this.size = size;
@@ -17,17 +21,41 @@ public class CircularStack {
 	}
 	
 	public synchronized int getNextIndice() {
+		int index = this.nextIndex;
 		this.nextIndex++;
 		if(this.nextIndex >= this.size) {
 			this.nextIndex = 0;
+			completed = true;
 		}
-		return this.nextIndex;
+		return index;
 	}
 	
-	public String[] getAll() {
-		String[] results = new String[this.size];
-		for(int i=0; i<this.size; i++) {
-			results[i] = this.arrays[i];
+	public List<String> getAllAscendant() {
+		List<String> results = new ArrayList<String>();
+		int index = this.nextIndex;
+		boolean completed = this.completed;
+		if(completed) {
+			for(int i=index; i<this.size; i++) {
+				results.add(this.arrays[i]);
+			}
+		}
+		for(int i=0; i<index; i++) {
+			results.add(this.arrays[i]);
+		}
+		return results;
+	}
+	
+	public List<String> getAllDescendant() {
+		List<String> results = new ArrayList<String>();
+		int index = this.nextIndex;
+		boolean completed = this.completed;
+		for(int i=index-1; i>=0; i--) {
+			results.add(this.arrays[i]);
+		}
+		if(completed) {
+			for(int i=this.size-1; i>=index; i--) {
+				results.add(this.arrays[i]);
+			}
 		}
 		return results;
 	}
---
+++ src/test/java/org/telosys/webtools/monitoring/CircularStackTest.java
@@ -0,0 +1,77 @@
+package org.telosys.webtools.monitoring;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class CircularStackTest {
+	
+	@Test
+	public void testAscendant() {
+		CircularStack circularStack = new CircularStack(3);
+		assertEquals(0, circularStack.getAllAscendant().size());
+		
+		circularStack.push("1");
+		List<String> results = circularStack.getAllAscendant();
+		assertEquals(1, results.size());
+		assertEquals("1", results.get(0));
+		
+		circularStack.push("2");
+		results = circularStack.getAllAscendant();
+		assertEquals(2, results.size());
+		assertEquals("1", results.get(0));
+		assertEquals("2", results.get(1));
+		
+		circularStack.push("3");
+		results = circularStack.getAllAscendant();
+		assertEquals(3, results.size());
+		assertEquals("1", results.get(0));
+		assertEquals("2", results.get(1));
+		assertEquals("3", results.get(2));
+		
+		circularStack.push("4");
+		results = circularStack.getAllAscendant();
+		assertEquals(3, results.size());
+		assertEquals("2", results.get(0));
+		assertEquals("3", results.get(1));
+		assertEquals("4", results.get(2));
+		
+		
+	}
+
+	@Test
+	public void testDescendant() {
+		CircularStack circularStack = new CircularStack(3);
+		assertEquals(0, circularStack.getAllDescendant().size());
+		
+		circularStack.push("1");
+		List<String> results = circularStack.getAllDescendant();
+		assertEquals(1, results.size());
+		assertEquals("1", results.get(0));
+		
+		circularStack.push("2");
+		results = circularStack.getAllDescendant();
+		assertEquals(2, results.size());
+		assertEquals("2", results.get(0));
+		assertEquals("1", results.get(1));
+		
+		circularStack.push("3");
+		results = circularStack.getAllDescendant();
+		assertEquals(3, results.size());
+		assertEquals("3", results.get(0));
+		assertEquals("2", results.get(1));
+		assertEquals("1", results.get(2));
+		
+		circularStack.push("4");
+		results = circularStack.getAllDescendant();
+		assertEquals(3, results.size());
+		assertEquals("4", results.get(0));
+		assertEquals("3", results.get(1));
+		assertEquals("2", results.get(2));
+		
+		
+	}
+	
+}
