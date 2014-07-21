59dc38f0f9e2ac9b005b08789facb345ff05ac57
ludo1026 <ludovic.chaboud@sogeti.com>
2014-07-21 17:32:35 +0200

    Top requests

--- src/main/java/org/telosys/webtools/monitoring/bean/RequestComparator.java
+++ src/main/java/org/telosys/webtools/monitoring/bean/RequestComparator.java
@@ -4,10 +4,46 @@ import java.util.Comparator;
 
 public class RequestComparator implements Comparator<Request> {
 
+	// true : natural order
+	// false: reverse order
+	private final boolean orderAscendant;
+	
+	public RequestComparator() {
+		this.orderAscendant = true;
+	}
+	
+	public RequestComparator(boolean orderAscendant) {
+		this.orderAscendant = orderAscendant;
+	}
+	
 	@Override
 	public int compare(Request r1, Request r2) {
-		
-		return 0;
+		int compareAscendant = compareAscendant(r1, r2);
+		if(orderAscendant) {
+			return compareAscendant;
+		} else {
+			return -compareAscendant;
+		}
+	}
+	
+	private int compareAscendant(Request r1, Request r2) {
+		if(r1 == null) {
+			if(r2 == null) {
+				return 0;
+			}
+			return -1;
+		}
+		if(r2 == null) {
+			return 1;
+		}
+		if(r1.getElapsedTime() == r2.getElapsedTime()) {
+			return 0;
+		}
+		if(r1.getElapsedTime() > r2.getElapsedTime()) {
+			return 1;
+		} else {
+			return -1;
+		}
 	}
 	
 }
--- src/main/java/org/telosys/webtools/monitoring/bean/TopRequests.java
+++ src/main/java/org/telosys/webtools/monitoring/bean/TopRequests.java
@@ -1,5 +1,6 @@
 package org.telosys.webtools.monitoring.bean;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
@@ -13,27 +14,62 @@ public class TopRequests {
 		if(size <= 0) {
 			throw new IllegalStateException("size of top requests list must be greater than 0");
 		}
-		requests = new Request[size-1];
+		requests = new Request[size];
 	}
 	
 	public void add(Request request) {
-		int pos = 0;
-		while(pos<requests.length
-		   && requests[pos] != null
-		   && requests[pos].getElapsedTime() >= request.getElapsedTime()) {
-			pos++;
+		if(mustBeInsert(request)) {
+			int posMinimum = getMinimumPosition();
+			requests[posMinimum] = request;
 		}
-		
-		if(pos < requests.length) {
+	}
+	
+	protected int getMinimumPosition() {
+		int posMinimum = -1;
+		long minimum = -1;
+		for(int pos=0; pos<requests.length; pos++) {
 			if(requests[pos] == null) {
-				requests[pos] = request;
-			} else {
-				for(int pos2 = pos; pos2<requests.length-1; pos2++) {
-					requests[pos2+1] = requests[pos2];
-				}
-				requests[pos] = request;
+				posMinimum = pos;
+				break;
+			}
+			if(requests[pos].getElapsedTime() < minimum || minimum == -1) {
+				posMinimum = pos;
+				minimum = requests[pos].getElapsedTime();
 			}
 		}
+		return posMinimum;
+	}
+	
+	protected boolean mustBeInsert(Request request) {
+		for(int pos=0; pos<requests.length; pos++) {
+			if(requests[pos] == null) {
+				return true;
+			}
+			if(request.getElapsedTime() > requests[pos].getElapsedTime()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public List<Request> getAllAscending() {
+		Set<Request> allSorted = new TreeSet<Request>(new RequestComparator());
+		for(Request request : requests) {
+			if(request != null) {
+				allSorted.add(request);
+			}
+		}
+		return new ArrayList<Request>(allSorted);
+	}
+
+	public List<Request> getAllDescending() {
+		Set<Request> allSorted = new TreeSet<Request>(new RequestComparator(false));
+		for(Request request : requests) {
+			if(request != null) {
+				allSorted.add(request);
+			}
+		}
+		return new ArrayList<Request>(allSorted);
 	}
 	
 }
